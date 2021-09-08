863c14de1dfcacf1669d3b59431722ad570c2e30
Shohei Aoki <shoaok@gmail.com>
2021-09-08 21:14:16 +0300

    temporal commit

--- ACO/colony.py
+++ ACO/colony.py
@@ -1,4 +1,22 @@
-
+from ant import Ant 
+from field import Field
+from variables import ANT_NUM
 class Colony():
     def __init__(self):
-        pass
+        self.field = Field()
+        self.ant= [Ant()]*ANT_NUM
+        # for i in range(ANT_NUM):
+        #     self.ant[i] = Ant(self)
+
+        self.nume = [0.0]*self.field.nodeNum
+        # for i in range(self.field.nodeNum):
+        #     self.nume[i] = [0.0]*self.field.nodeNum
+
+    def selectRoute():
+        for i in range(self.field.nodeNum):
+            for j in range(i):
+                nume[i][j] = pow(field.)
+
+
+
+        
