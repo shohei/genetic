6b74cc4b16838567c216da5d2c200b8f4624c85d
Shohei Aoki <shoaok@gmail.com>
2021-09-09 17:25:38 +0300

    temporal commit

--- ABC/dataset.py
+++ ABC/dataset.py
@@ -1,7 +1,19 @@
+import pandas as pd
+
 
 class Dataset():
-    def __init__(self):
-        pass
+    def __init__(self, filename):
+        df = pd.read_csv(filename, header=Non)
+
+        self.exData = []
+        self.exSData = []
+        self.resData = []
+        self.resSData = []
+        self.exSd = []
+        self.coef = []
+
+        self.resAve = 0.0
+        self.resSd = 0.0
 
     def setCoef(self, sCoef):
         pass
